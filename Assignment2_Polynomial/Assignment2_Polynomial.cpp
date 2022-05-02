b7094f65049d699ebf388a971fa30b876659aa36
Alisa Illarionova <illarionova.a@live.unic.ac.cy>
2022-05-02 09:19:51 +0300

    Attempt at linked lists

--- Assignment2_Polynomial/Assignment2_Polynomial.cpp
+++ Assignment2_Polynomial/Assignment2_Polynomial.cpp
@@ -1,58 +1,104 @@
 //Program to add two polynomials represented in linkedlist using recursion
 #include<iostream>
 #include <string>
+#include <malloc.h>
 using namespace std;
 
-void addPolynomial(string poly1, string poly2);
+struct Polynomial {
+	int coeff;
+	int expo;
+	Polynomial* next;
+	Polynomial(int coeff, int expo)
+	{
+		this->coeff = coeff;
+		this->expo = expo;
+		this->next = NULL;
+	}
+};
 
-int main(){
-
-	string p1 = "7x5+3x";
-	cout << p1 << endl;
-	string p2 = "7x5+2x5+7x";
-	cout << p2 << endl;
-	char* str1 = &p1[0];
-	char* str2 = &p2[0];
+void addPolynomials(Polynomial* head1, Polynomial* head2);
+void insert(Polynomial* head, int coeff, int power);
+void printList(Polynomial* head);
+int findCoeff(string s, int size, int coeff);
 
 
-	int coeff, expo, size;
-	string tempC, tempE;
-	cout << strncmp(str1, str2, 3) << endl;
+int main() {
 
+	string p1, p2, tempC, tempE;
+	int size, coeff, expo;
+	//Polynomial* head;
+	//Polynomial* head2;
+	cout << "First polynomial: ";
+	getline(cin, p1);
+	for (size = 0; p1[size] != '\0'; size++);
 
-	//char *ptr;
-
-	//cout << "First polynomial: ";
-	//getline(cin, p1);
-	for (size = 0; p1[size] !='\0'; size++);
-	for (int i = 0; i < size;i++) {
-		if (p1[i] == 'x')
-		tempC = 1;
+	for (int i = 0; i < size; i++) {
+		if (p1[i] == 'x') {
+			coeff = 1;
+			return coeff; //retun coeff =1;
+		}
 		else if (p1[i] != 'x' && p1[i + 1] == 'x') {
 			tempC = p1[i];
-			//tempC[1=
-
+			coeff = stoi(tempC);
+			return coeff;
 		}
-		
-	}
-	//getline(cin, p1);
 
+		expo = stoi(tempE);
+		Polynomial* head = new Polynomial(coeff, expo);
+	}
 	//cout << "Second polynomial: ";
 	//getline(cin, p2);
-	for (size = 0; p2[size] != '\0'; size++);
-	addPolynomial(p1, p2);
-
-
-	return 0;
+	//Polynomial* head = new Polynomial(5, 2);
+	insert(head, 4, 1);
+	Polynomial* head2 = new Polynomial(6, 2);
+	insert(head2, 4, 1);
+	//printList(head);
+	cout << endl;
+	printList(head2);
+	cout << endl <<
+		"Addition:" << endl;
+	addPolynomials(head, head2);
 }
 
-void addPolynomial(string poly1, string poly2) {
+void addPolynomials(Polynomial* head1, Polynomial* head2) {
+	if (head1 == NULL && head2 == NULL)
+		return ;
+	else if (head1->expo == head2->expo) {
+		cout << " " << head1->coeff + head2->coeff <<
+			"x^" << head1->expo << " ";
+		addPolynomials(head1->next, head2->next);
+	}
+	else if (head1->expo > head2->expo) {
+		cout << " " << head1->coeff <<
+			"x^" << head1->expo << " ";
+		addPolynomials(head1->next, head2);
+	}
+	else {
+		cout << " " << head2->coeff <<
+			"x^" << head2->expo << " ";
+		addPolynomials(head1, head2->next);
+	}
+}
 
-	string p3;
-	p3.clear();
-	int i = 0, j = 0;
+void insert(Polynomial* head, int coeff, int power) {
+	Polynomial* new_node = new Polynomial(coeff, power);
 
-	
+	while (head->next != NULL)
+		head = head->next;
+	head->next = new_node;
+}
 
+void printList(Polynomial* head) {
+	cout << "Linked List" << endl;
+	while (head != NULL) {
+		cout << " " << head->coeff <<
+			"x" << "^" << head->expo;
+		head = head->next;
+	}
+}
 
+int findCoeff(string s, int size, int coeff)
+{
+
+	return coeff;
 }
