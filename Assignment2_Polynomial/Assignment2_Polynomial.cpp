14d1c364053cfcadea6f9a7621b54d9b956d262e
Alisa Illarionova <illarionova.a@live.unic.ac.cy>
2022-05-02 11:19:58 +0300

    All powers work

--- Assignment2_Polynomial/Assignment2_Polynomial.cpp
+++ Assignment2_Polynomial/Assignment2_Polynomial.cpp
@@ -4,47 +4,52 @@
 using namespace std;
 
 int main() {
-
+	
 	double polynomial1[10] = {};
 	double polynomial2[10] = {};
 	char c;
 	double coefficient = 0;
 	int power = 0;
 	int counter = 0;
-	int boolNextPower = 0; //set to false
+	bool nextPower = false; //set to false
 
 	cout << "Enter first polynomial: ";
 	while (c = cin.get(), c != '\n') {
+
 		if (c == '+' || c == '-') {
-			if (boolNextPower == 1 && coefficient==NULL) {
+			if (nextPower && coefficient == NULL) {
 				polynomial1[1] = 1;
 			}
-
-			else if (boolNextPower == 1 && coefficient!=NULL) {
+			else if (nextPower && coefficient!=NULL) {
 				polynomial1[1] = coefficient;
 			}
-			//else if (boolNextPower == 0)
-				//polynomial1[0] = coefficient;
-			else boolNextPower = 0;
+			else if (!nextPower && coefficient != NULL) {
+				polynomial1[0] = coefficient;
+			}
+			else
+				nextPower=false;
 		}
 		else if (c == 'x' || c == 'X')
-			boolNextPower = 1;
+			nextPower=true;
 		else if (c >= '0' && c <= '9') {
 			cin.unget();
-			if (boolNextPower == 1) {
+
+			if (nextPower) {
 				cin >> power;
 				polynomial1[power] = coefficient;
-				boolNextPower=0;
+				nextPower=false;
 				coefficient = NULL;
 			}
-			//else if (boolNextPower == 0 && c == '+' || boolNextPower == 0 && c == '-') {
-				//polynomial1[1] = coefficient;
-			//}
-
+			
 			else {
 				cin >> coefficient;
 			}
 		}
+	}
+
+	if (c == '\n' && coefficient != NULL) {
+		if(nextPower) polynomial1[1] = coefficient;
+		else polynomial1[0] = coefficient;
 	}
 
 	for (int i = 9; i >= 0; i--) {
