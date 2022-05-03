d0470eb6f434f8f8b89d3a4f29cb3675d4e1553a
Alisa <alisa.illarionova@gmail.com>
2022-05-03 20:03:06 +0300

    Function initialiseArray works, but polynomial1 is initialised and stored only inside the function. Array is returned but is lost after that

--- Assignment2_Polynomial/Assignment2_Polynomial.cpp
+++ Assignment2_Polynomial/Assignment2_Polynomial.cpp
@@ -1,166 +1,85 @@
-/**
-* file Assignment2_polynomial.cpp
-* \brief Program to add two string polynomials through arrays
-* \details 
-* \author Alisa Illarionova
-* \date 3/5/2022
-*/
+//Program to add two string polynomials through arrays
 #include <iostream>
 #include <string>
-#include <cassert>
 using namespace std;
-bool checkSign(char c); //bool function that flags negative sign
+const int MAX = 10;
+bool nextPower = false;//set to false
+bool isSignNeg = false;
 
-/**
-*Function <code>main</code> this is the driver of the program
-*<BR>
-* @return Returns 0
-*/
+double initialiseArray();
+//double initialiseArray(double array);
+
+
 int main() {
-	double polynomial1[10] = {}; //Array with coefficients of first polynomial
-	double polynomial2[10] = {}; //                        of second polynomial
-	double sum[10] = {};		 //                        of the sum
-	char c;                      // Symbol  
-	double coefficient = 0;      // Coefficient of polynomial terms
-	int power = 0;				 // Power(exponent) of polynomial terms
-	int count = 0;               // Counter
-	bool nextPower = false;      // set to false
-	bool isSignNeg = false;      // set to false
 
-	cout << "\nEnter first polynomial: ";
-	while (c = cin.get(), c != '\n') {  // Start reading input character by character until '\n' is reached 
-		if (c == '+' || c == '-') {
-			isSignNeg = checkSign(c);   // Returns true if sign is '-'
+	//char c;
+	//double polynomial1[MAX] = {};
+	double polynomial2[MAX] = {};
 
-			if (nextPower && coefficient == NULL) { // If we expect the power and coeffiecient = 0 
-				polynomial1[1] = 1;
-				nextPower = false;      // resetting nextPower to false; moving to the next term
-										// we put coefficient of the current term in the array
-			}
-			else if (nextPower && coefficient != NULL) {  // If we expect the power and coefficient!=0
-				polynomial1[1] += coefficient;
-				coefficient = NULL;     // resetting coefficent
-				nextPower = false;      // resetting nextPower, moving on to the next term
-			}
-			else if (!nextPower && coefficient != NULL) { // Finding constant: We don't expect power and we have coefficient stored
-				polynomial1[0] += coefficient;	//x^0 is just constant
-				coefficient = NULL;		// resetting coefficient 
-			}
-			else
-				nextPower = false;		// new term, next umber will be coefficient not power  
-		}
-		else if (c == 'x' || c == 'X') {
-			if (coefficient == NULL)    // no number before x, i.e. x=1x
-				if (isSignNeg) coefficient = -1; 
-				else coefficient = 1;
-			nextPower = true;
+	double balance[5] = {};
+	double* p;
+	p = balance;
 
-		}
-		else if (c >= '0' && c <= '9') {
-			cin.unget();
+	cout << "Enter first polynomial: ";
+	//initialiseArray(polynomial1[MAX-1]);//???
+	initialiseArray();
 
-			if (nextPower) {
-				//do {
-					//cout << "Power must be an integer!";
-				cin >> power;
-				//}while (power %1!=0);
+	//for (int i = 9; i > 0; i--) {
+		//if (polynomial1[i] != 0)
+			//cout << "coefficient = " << polynomial1[i] << "   power = " << i << endl;
+	//}
 
-				if (polynomial1[power] != 0 && isSignNeg) {  // Addition of coefficients for multiple terms of same exponents 
-					if (coefficient < 0) {					 
-						polynomial1[power] += coefficient;
-					}
-					else {
-						if (polynomial1[power] > coefficient)
-							polynomial1[power] -= coefficient;
-						else polynomial1[power] = coefficient - polynomial1[power];
-					}
-				}
-				else if (polynomial1[power] != 0 && !isSignNeg)	
-					polynomial1[power] += coefficient;
-				else
-					polynomial1[power] = coefficient;		// First time entering coefficient for the exponent
-				nextPower = false;							// Resetting nextPower and coefficient
-				coefficient = NULL;
-			}
+	return 0;
+}
 
-			else {
-				if (isSignNeg) {							// Make coefficent negative if sign is '-'
-					cin >> coefficient;
-					coefficient *= -1;
-				}
-				else cin >> coefficient;					// Store number as coefficient
-			}
-		}
-	}
-
-	if (c == '\n' && coefficient != NULL) {					// Reached the end of the line and have unused coefficient
-		if (nextPower) {									// Expect a power, then we have coefficient for x^1 
-			if (polynomial1[1] != 0) polynomial1[1] += coefficient;
-			//else polynomial1[1] = coefficient;
-		}
-		else												// Don't expect a power, have a constant, i.e. x^0 coefficient
-			if (polynomial1[0] != 0) polynomial1[0] += coefficient; 
-			//else 
-			//polynomial1[0] += coefficient;
-	}
-
-	for (int i =0; i<10;i++)
-		cout << polynomial1[i] << ' ';			//print out coefficients @todo remove
-
-	cout << "\nEnter second polynomial: ";		//Ask for second polynomial
-	coefficient = 0;
-	power = 0;
-	nextPower = false;
-	isSignNeg = false;							// Reset all the variables to zero/false
+//double initialiseArray(double array) {
+double initialiseArray() {
+	double polynomial1[MAX] = {};
+	double coefficient = 0;
+	int power = 0;
+	char c;
+	double* pointer;
+	pointer = polynomial1;
 	while (c = cin.get(), c != '\n') {
-
 		if (c == '+' || c == '-') {
 
-			isSignNeg = checkSign(c);
+			if (c == '+') isSignNeg = false;
+			else if (c == '-') isSignNeg = true;
 
-			if (nextPower && coefficient == NULL) {
-				polynomial2[1] = 1;
-				nextPower = false;
+			else if (nextPower && coefficient == NULL) {
+				*(pointer + 1) = 1;
 			}
 			else if (nextPower && coefficient != NULL) {
-				polynomial2[1] = coefficient;
-				coefficient = NULL;
-				nextPower = false;
+				*(pointer + 1) = coefficient;
 			}
 			else if (!nextPower && coefficient != NULL) {
-				polynomial2[0] = coefficient;
-				coefficient = NULL;
+				*pointer = coefficient;
 			}
 			else
 				nextPower = false;
 		}
-		else if (c == 'x' || c == 'X') {
-			if (coefficient == NULL)
-				if (isSignNeg) coefficient = -1;
-				else coefficient = 1;
+		else if (c == 'x' || c == 'X')
 			nextPower = true;
-		}
 		else if (c >= '0' && c <= '9') {
 			cin.unget();
 
+
 			if (nextPower) {
 				cin >> power;
-				//assert(power > 0); @todo remove
-				if (polynomial2[power] != 0 && isSignNeg) {
+				if (*pointer != 0 && isSignNeg) {
 					if (coefficient < 0) {
-						polynomial2[power] += coefficient;
+						*(pointer + power) += coefficient;
 					}
 					else {
-						if (polynomial2[power] > coefficient)
-							polynomial2[power] -= coefficient;
-						else polynomial2[power] = coefficient - polynomial2[power];
+						if (*(pointer + power) > coefficient)
+							*(pointer + power) -= coefficient;
+						else *(pointer + power) = coefficient - *(pointer + power);
 					}
-					coefficient = NULL;
 				}
-				else if (polynomial2[power] != 0 && !isSignNeg)
-					polynomial2[power] += coefficient;
+				else if (*(pointer + power) != 0 && !isSignNeg)
+					*(pointer + power) += coefficient;
 				else
-					polynomial2[power] = coefficient;
+					*(pointer + power) = coefficient;
 				nextPower = false;
 				coefficient = NULL;
 			}
@@ -173,57 +92,16 @@ int main() {
 				else cin >> coefficient;
 			}
 		}
-	}
 
-	if (c == '\n' && coefficient != NULL) {
-		if (nextPower) {
-			if (polynomial2[1] != 0) polynomial2[1] += coefficient;
-			else polynomial2[1] = coefficient;
-		}
-		else
-			if (polynomial2[0] != 0) polynomial2[0] += coefficient;
-			else polynomial2[0] = coefficient;
-	}
-
-	for (int i = 0; i < 10; i++)
-		cout << polynomial2[i] << ' ';
-	cout << endl;//@todo remove
-
-	for (int i = 0; i < 10; i++) {						// Fill in sum array by adding coefficients of 2 polynomials
-			sum[i] = polynomial1[i] + polynomial2[i];
+		if (c == '\n' && coefficient != NULL) {
+			if (nextPower) *(pointer + 1) = coefficient;
+			else *pointer = coefficient;
 		}
 
-	cout << "\nSum of polynomials = ";                  // Print out sum array
+	}
 	for (int i = 9; i >= 0; i--) {
-		if (sum[i] != 0) {								// Print only non-zero terms
-			if (i == 0) {								// Printing constant
-				if (sum[i] > 0 && count != 0)			// Only print '+' for positive term only when not first term
-					cout << '+' << sum[i];
-				else cout << sum[i];
-			}
-			else if (i == 1) {							// Print x^1	
-				if (sum[i] > 0 && count != 0) cout << '+' << sum[i] << 'x';
-				else if (sum[1] == 1) cout << 'x';
-				else if (sum[1] == -1) cout << '-x';
-				else cout << sum[i] << 'x';
-			}
-			else {
-				if (sum[i] > 0 && count != 0) cout << '+' << sum[i] << 'x' << i;
-				else cout << sum[i] << 'x' << i;
-			}
-			count++;									// Needed to print out first positive term without '+'
-		}
+		if (*(pointer + i) != 0)
+			cout << "coefficient = " << *(pointer + i) << "   power = " << i << endl;
 	}
-	return 0;
-}
-
-/**
-*Function <code>checkSign</code> checks if the character is '-' and so coefficient is negative
-* <BR>
-* @param char Symbol to check 
-* @return Returns <code>false</code> if char is '+' and <code>true</code> if char is '-'
-*/
-bool checkSign(char c) {
-	if (c == '+') return false;
-	else if (c == '-') return true;
+	return polynomial1[MAX-1];//????
 }
